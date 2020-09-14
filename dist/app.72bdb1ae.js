56f2bee00b957aef8317f379a431a7dc62f94db1
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-09-14 18:49:32 -0400

    created HTML for contact section

--- dist/app.72bdb1ae.js
+++ dist/app.72bdb1ae.js
@@ -118,11 +118,35 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"../ts/app.js":[function(require,module,exports) {
-var $dropdownMenu = $('main ul#dropdownMenu');
-$('i.fas.fa-bars').on('click', function () {
-  console.log('clicked Hamburglar');
+console.log($);
+var $dropdownMenu = $('header ul#dropdownMenu');
+var $hamburgerButton = $('i.fas.fa-bars');
+$hamburgerButton.on('click', function () {
   $dropdownMenu.slideToggle(500);
-});
+}); //Found this function here: bootstrap-menu.com/detail-smart-hide.html
+// the way it works is by checking for the navbar's height
+// add padding top to show content behind navbar
+
+https: $('body').css('padding-top', $('.navbar').outerHeight() + 'px'); // detect scroll top or down
+
+
+if ($('.smart-scroll').length > 0) {
+  // check if element exists
+  var last_scroll_top_1 = 0;
+  $(window).on('scroll', function () {
+    var scroll_top = $(this).scrollTop();
+
+    if (scroll_top < last_scroll_top_1) {
+      $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+    } else {
+      $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+    }
+
+    last_scroll_top_1 = scroll_top;
+  });
+} else {
+  $hamburgerButton.css('display', 'none');
+}
 },{}],"../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- ts/app.ts
+++ ts/app.ts
@@ -1,6 +1,31 @@
-const $dropdownMenu = $('main ul#dropdownMenu');
+console.log($);
 
-$('i.fas.fa-bars').on('click', () => {
-	console.log('clicked Hamburglar');
+const $dropdownMenu = $('header ul#dropdownMenu');
+const $hamburgerButton = $('i.fas.fa-bars');
+
+$hamburgerButton.on('click', () => {
 	$dropdownMenu.slideToggle(500);
 });
+
+//Found this function here: bootstrap-menu.com/detail-smart-hide.html
+// the way it works is by checking for the navbar's height
+
+// add padding top to show content behind navbar
+https: $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+
+// detect scroll top or down
+if ($('.smart-scroll').length > 0) {
+	// check if element exists
+	let last_scroll_top = 0;
+	$(window).on('scroll', function () {
+		let scroll_top = $(this).scrollTop();
+		if (scroll_top < last_scroll_top) {
+			$('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+		} else {
+			$('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+		}
+		last_scroll_top = scroll_top;
+	});
+} else {
+	$hamburgerButton.css('display', 'none');
+}
