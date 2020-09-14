593cb914b945775e419d4a818e064a707a8282f7
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-09-14 11:03:48 -0400

    finished hamburger menu

--- ts/app.js
+++ ts/app.js
@@ -1,14 +1,5 @@
 var $dropdownMenu = $('main ul#dropdownMenu');
-$('i.fas.fa-bars').click(function () {
-    console.log('clicked Hamburger');
-    if ($dropdownMenu.css('opacity') === '0') {
-        console.log('hidden');
-        $dropdownMenu.css('opacity', '1');
-        $dropdownMenu.css('height', 'fit-content');
-    }
-    else {
-        console.log('show');
-        $dropdownMenu.css('opacity', '0');
-        $dropdownMenu.css('height', '0');
-    }
+$('i.fas.fa-bars').on('click', function () {
+    console.log('clicked Hamburglar');
+    $dropdownMenu.slideToggle(500);
 });
--- ts/app.ts
+++ ts/app.ts
@@ -1,14 +1,6 @@
 const $dropdownMenu = $('main ul#dropdownMenu');
 
-$('i.fas.fa-bars').click(() => {
-	console.log('clicked Hamburger');
-	if ($dropdownMenu.css('opacity') === '0') {
-		console.log('hidden');
-		$dropdownMenu.css('opacity', '1');
-		$dropdownMenu.css('height', 'fit-content');
-	} else {
-		console.log('show');
-		$dropdownMenu.css('opacity', '0');
-		$dropdownMenu.css('height', '0');
-	}
+$('i.fas.fa-bars').on('click', () => {
+	console.log('clicked Hamburglar');
+	$dropdownMenu.slideToggle(500);
 });
