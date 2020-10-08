b541d865a2d987d6b70fc422d70ccdcdae530efd
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-08 11:31:11 -0400

    local commits 10/8
    pre-firebase deploy of new bootstrap menu

--- dist/app.5ff847cd.js
+++ dist/app.5ff847cd.js
@@ -576,7 +576,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52684" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65125" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
